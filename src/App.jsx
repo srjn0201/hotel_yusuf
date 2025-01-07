615a98a1cffd586de1373072e8dd23c2d2a56e79
mohd sharjeel <srjn0201@gmail.com>
2025-01-07 18:44:46 +0530

    finishing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+import Home from './Components/Home'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hotel yusuf</h1>
+      <Home />
     </>
   )
 }
